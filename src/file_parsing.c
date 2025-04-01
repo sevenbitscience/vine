5486cd2147ae7391a0f7f4ff900b80d79d08dcbb
Joey Milausnic <milausnic@gmail.com>
2025-04-01 14:56:19 -0400

    Remove unused file parsing functions

--- src/file_parsing.c
+++ src/file_parsing.c
@@ -5,14 +5,6 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 
-int isMarkdown(const char* filename) {
-	if (strlen(filename) < 3) {
-		return 0;
-	} else {
-		return filename[strlen(filename)-2] == 'm' && filename[strlen(filename)-1] == 'd';
-	}
-}
-
 int isDirectory(const char *path) {
    struct stat statbuf;
    if (stat(path, &statbuf) != 0)
@@ -40,48 +32,6 @@ int getFileCount(const char* path) {
 	return fileCount;
 }
 
-int getCourseCount(const char* path) {
-	DIR *directory;						// The directory we are searching in
-	struct dirent *file;				// A directory entry
-	int fileCount = 0;					// So we know how many markdown files there are
-
-	// First count up the number of markdown files, so we can allocate enough memory to store their names
-	directory = opendir(path);
-	if (directory != NULL) {
-		while (file = readdir(directory)) {
-			if (file->d_name[0] != '.' && isDirectory(file->d_name)) {		// Ignore hidden files
-				fileCount++;
-			}
-		}
-		(void) closedir(directory);
-	} else {
-		perror("Couldn't open the directory");
-	}
-
-	return fileCount;
-}
-
-int getMarkdownCount(const char* path) {
-	DIR *directory;						// The directory we are searching in
-	struct dirent *file;				// A directory entry
-	int fileCount = 0;					// So we know how many markdown files there are
-
-	// First count up the number of markdown files, so we can allocate enough memory to store their names
-	directory = opendir(path);
-	if (directory != NULL) {
-		while (file = readdir(directory)) {
-			if (file->d_name[0] != '.' && isMarkdown(file->d_name)) {		// Ignore hidden files
-				fileCount++;
-			}
-		}
-		(void) closedir(directory);
-	} else {
-		perror("Couldn't open the directory");
-	}
-
-	return fileCount;
-}
-
 void GetFiles(char* files[], char* path) {
 	DIR *directory;
 	struct dirent *file;
@@ -95,56 +45,6 @@ void GetFiles(char* files[], char* path) {
 			strcpy(filepath, path);
 			strcat(filepath, file->d_name);
 			if (file->d_name[0] != '.') {		// Ignore hidden files
-				files[i] = malloc(sizeof(char*));
-				strcpy(files[i], file->d_name);
-				i++;
-			}
-			free(filepath);
-		}
-		(void) closedir(directory);
-	} else {
-		perror("Couldn't open the directory");
-	}
-}
-
-void GetCourses(char* courses[], char* path) {
-	DIR *directory;
-	struct dirent *file;
-	int i = 0;
-	char* filepath;
-
-	directory = opendir(path);
-	if (directory != NULL) {
-		while (file = readdir(directory)) {
-			filepath = malloc(sizeof(path) + sizeof(file->d_name) + 1);		
-			strcpy(filepath, path);
-			strcat(filepath, file->d_name);
-			if (file->d_name[0] != '.' && isDirectory(filepath)) {		// Ignore hidden files
-				courses[i] = malloc(sizeof(char*));
-				strcpy(courses[i], file->d_name);
-				i++;
-			}
-			free(filepath);
-		}
-		(void) closedir(directory);
-	} else {
-		perror("Couldn't open the directory");
-	}
-}
-
-void GetNotes(char* files[], char* path) {
-	DIR *directory;
-	struct dirent *file;
-	int i = 0;
-	char* filepath;
-
-	directory = opendir(path);
-	if (directory != NULL) {
-		while (file = readdir(directory)) {
-			filepath = malloc(sizeof(path) + sizeof(file->d_name) + 1);		
-			strcpy(filepath, path);
-			strcat(filepath, file->d_name);
-			if (file->d_name[0] != '.' && isMarkdown(filepath)) {		// Ignore hidden files
 				files[i] = malloc(sizeof(char*));
 				strcpy(files[i], file->d_name);
 				i++;
--- src/file_parsing.h
+++ src/file_parsing.h
@@ -2,16 +2,6 @@
 #define FILE_PARSING_H
 
 /**
- * Checks if a provided string is the name of a markdown file.
- *
- * Checks if the final three characters of the filename are '.md'
- *
- * @param filename A string
- * @return Returns 1 true if the provided filename is a markdown file.
- */
-int isMarkdown(const char* filename);
-
-/**
  * Checks if the object at a path is a directory
  *
  * @param path Path to the thing to check 
@@ -27,20 +17,6 @@ int isDirectory(const char *path);
 int getFileCount(const char* path);
 
 /**
- * Counts the number of directories in a parent directory
- *
- * @param path Path to the parent directory to search in
- */
-int getCourseCount(const char* path);
-
-/**
- * Counts the number of markdown files in a parent directory
- *
- * @param path Path to the parent directory to search in
- */
-int getMarkdownCount(const char* path);
-
-/**
  * Create an array of file or directory names
  *
  * Generic function for creating an array of objects in a directory
@@ -49,28 +25,5 @@ int getMarkdownCount(const char* path);
  * @param path The path to search at
  */
 void GetFiles(char* files[], char* path);
-
-/**
- * Create an array of course names
- *
- * The notes for each course are stored in a folder
- * the name of that folder is assumed to be the name of the course
- *
- * @param courses The array that the course names will be added to
- * @param path The path the course folders are stored at
- */
-void GetCourses(char* courses[], char* path);
-
-/**
- * Create an array of markdown notes file's names
- *
- * The notes for each course are stored in a folder
- * This function looks at a directory and add all of the names of markdown
- * files to an array
- *
- * @param courses The array that the markdown file names will be added to
- * @param path The path the notes are stored at
- */
-void GetNotes(char* files[], char* path);
 
 #endif
