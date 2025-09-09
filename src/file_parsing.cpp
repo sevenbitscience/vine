9637c6a6a08256a4e1747fa4c9ef897cfe0a085b
Joeseph Milausnic <joseph.milausnic.24@cnu.edu>
2025-09-09 15:35:52 -0400

    Complete cpp refactor on file_parsing

--- src/file_parsing.cpp
+++ src/file_parsing.cpp
@@ -1,34 +1,8 @@
 #include "file_parsing.h"
-#include <filesystem>
-#include <stdexcept>
 
 namespace fs = std::filesystem;
 
-/*
 int getFileCount(std::string path) {
-	DIR *directory;						// The directory we are searching in
-	struct dirent *file;				// A directory entry
-	int fileCount = 0;
-
-	directory = opendir(path);
-	if (directory != NULL) {
-		while ((file = readdir(directory)) != NULL) {
-			if (file->d_name[0] != '.') {		// Ignore hidden files
-				fileCount++;
-			}
-		}
-		(void) closedir(directory);
-	} else {
-		perror("Couldn't open the directory");
-	}
-
-	return fileCount;
-}
-*/
-
-std::vector<fs::directory_entry> GetFiles(std::string path) {
-	std::vector<fs::directory_entry> files;
-
 	fs::directory_entry path_dirent{fs::path{path}};
 
 	if (!path_dirent.exists()) 
@@ -37,22 +11,26 @@ std::vector<fs::directory_entry> GetFiles(std::string path) {
 	if (!path_dirent.is_directory()) 
 		throw std::invalid_argument("Directory path does not refer to a directory!");
 
+	int count = 0;
+	for (const auto& _: fs::directory_iterator(path)) {
+		count++;
+	}
+	return count;
+}
+
+std::vector<fs::directory_entry> GetFiles(std::string path) {
+	fs::directory_entry path_dirent{fs::path{path}};
+
+	if (!path_dirent.exists()) 
+		throw std::invalid_argument("Directory path does not exist!");
+
+	if (!path_dirent.is_directory()) 
+		throw std::invalid_argument("Directory path does not refer to a directory!");
+
+	std::vector<fs::directory_entry> files;
+
 	for (const auto& dirent : fs::directory_iterator(path)) {
 		files.push_back(dirent);
 	}
 	return files;
 }
-
-/*
-char* concatPath(std::string path, std::string item) {
-	char* out = NULL;
-	if (path[(strlen(path)/sizeof(char))-1] == '/') {		// Does the path end in a forward slash?
-		out = (char*)malloc(sizeof(char) * (strlen(path) + strlen(item) + 0));
-		sprintf(out, "%s%s", path, item);
-	} else {									// We need to add a forward slash to the path
-		out = (char*)malloc(sizeof(char) * (strlen(path) + strlen(item) + 1));
-		sprintf(out, "%s/%s", path, item);
-	}
-	return out;
-}
-*/
--- src/file_parsing.h
+++ src/file_parsing.h
@@ -1,32 +1,26 @@
 #ifndef FILE_PARSING_H
 #define FILE_PARSING_H
 
-#include <iostream>
 #include <stdexcept>
 #include <string>
 #include <vector>
 #include <filesystem>
 
 /**
- * Counts the number of files and directories in a directory
+ * Counts the number of files and directories in a directory.
  *
  * @param path Path to the directory to search in
  */
 int getFileCount(std::string path);
 
 /**
- * Create an array of file or directory names
+ * Create an array of file or directory names.
  *
- * Generic function for creating an array of objects in a directory
+ * Generic function for creating an array of objects in a directory.
  *
- * @param path The path to search at
- * @return char** containing filenames of the files at path
+ * @param path The path to search at.
+ * @return char** containing filenames of the files at path.
  */
 std::vector<std::filesystem::path> GetFiles(std::filesystem::path path);
-
-/**
- * Concat parts of a file path
- */
-char* concatPath(std::string path, std::string item);
 
 #endif
